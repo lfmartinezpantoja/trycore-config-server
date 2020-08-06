8e6d742d718ad7bbed8f5cd02f213b8da4487785
fernando94 <lfmartinezpantoja@gmail.com>
2020-08-06 15:52:23 -0500

    full commit

---
+++ src/test/java/com/hiberus/config/HiberusConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiberus.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberusConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
